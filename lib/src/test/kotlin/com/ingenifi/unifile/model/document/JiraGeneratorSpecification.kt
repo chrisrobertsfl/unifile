874afbe3aaeca31dbe972dfff9f3ac9be52898c3
Christopher Roberts <chrisrobertsfl@gmail.com>
2024-01-27 15:44:52 -0500

    spike

--- lib/src/test/kotlin/com/ingenifi/unifile/model/document/JiraGeneratorSpecification.kt
+++ lib/src/test/kotlin/com/ingenifi/unifile/model/document/JiraGeneratorSpecification.kt
@@ -11,17 +11,27 @@ import kotlinx.coroutines.runBlocking
 import java.io.File
 
 class JiraGeneratorSpecification : StringSpec({
-
-    "generate story" {
-        val config = Config(keywordExtractor = KeywordExtractor(), verbosity = mockk<Verbosity>(), parameterStore = ParameterStore.loadProperties())
-        val sections = JiraGenerator(config = config, number = 1, file = resourceAsFile(name = "story", extension = "jira")).generate()
+    val config = Config(keywordExtractor = KeywordExtractor(), verbosity = mockk<Verbosity>(), parameterStore = ParameterStore.loadProperties())
+    fun generate(jiraType: String) = JiraGenerator(config = config, number = 1, file = resourceAsFile(name = jiraType, extension = "jira")).generate()
+    fun validate(jiraType: String, sections: List<Section>, output: Boolean = false) {
         val actual = DocumentGenerator(Document(sections = sections)).generate()
-        val expected = resourceAsString("expected-story-document.txt")
-        actual.output("actual")
-        expected.output("expected")
+        val expected = resourceAsString("expected-${jiraType}-document.txt")
+        if (output) {
+            actual.output("actual")
+            expected.output("expected")
+        }
         actual shouldBe expected
     }
+
+    "generate story" {
+        validate("story", generate("story"))
+    }
+
+    "generate spike" {
+        validate("spike", generate("spike"))
+    }
 })
+
 
 data class JiraGenerator(val config: Config, val number: Int, val file: File) : SectionGenerator {
     private val api = JiraApi(config.client, config.parameterStore.getParameter("jiraBaseUrl"), config.parameterStore.getParameter("apiToken"))
@@ -35,20 +45,24 @@ data class JiraGenerator(val config: Config, val number: Int, val file: File) : 
         val issueData = IssueData.from(issueKey, api.getIssue(issueKey))
         return when (issueData.type) {
             "story" -> JiraIssue.story(issueData)
+            "spike" -> JiraIssue.spike(issueData)
             else -> throw IllegalArgumentException("type '${issueData.type}' not yet supported")
         }
     }
-    private fun createSection(jiraIssue: JiraIssue): List<Section> = when(jiraIssue) {
+
+    private fun createSection(jiraIssue: JiraIssue): List<Section> = when (jiraIssue) {
         is Story -> SectionCreator.StoryCreator(story = jiraIssue, keywordExtractor = config.keywordExtractor).create(number)
+        is Spike -> SectionCreator.SpikeCreator(spike = jiraIssue, keywordExtractor = config.keywordExtractor).create(number)
+
         else -> throw IllegalArgumentException("type not yet supported")
     }
 }
 
 sealed interface SectionCreator {
-    fun create(number : Int) : List<Section>
+    fun create(number: Int): List<Section>
 
-    data class StoryCreator(val story : Story, val keywordExtractor: KeywordExtractor) : SectionCreator {
-        override fun create(number : Int): List<Section> {
+    data class StoryCreator(val story: Story, val keywordExtractor: KeywordExtractor) : SectionCreator {
+        override fun create(number: Int): List<Section> {
             val headingName = Name("Jira Story")
             val sectionNumber = SectionNumber(number)
             val title = Title(story.title)
@@ -59,10 +73,25 @@ sealed interface SectionCreator {
             return listOf(section)
         }
     }
+
+    data class SpikeCreator(val spike: Spike, val keywordExtractor: KeywordExtractor) : SectionCreator {
+        override fun create(number: Int): List<Section> {
+            val headingName = Name("Jira Spike")
+            val sectionNumber = SectionNumber(number)
+            val title = Title(spike.title)
+            val heading = Heading(headingName = headingName, sectionNumber = sectionNumber, title = title)
+            val keywords = KeywordsText.Keywords(keywordExtractor.extract(spike.detail))
+            val text = UnifileBodyText(headingName, keywords = keywords, detail = DetailText.Detail(spike.detail))
+            val section = Section(heading = heading, text = text)
+            return listOf(section)
+        }
+    }
 }
+
 data class IssueData(private val rawMap: Map<String, Any>?, val key: String, val type: String, val detail: String) {
 
     fun getStoryTitle() = rawMap?.get("summary") as String
+    fun getSpikeTitle() = rawMap?.get("summary") as String
 
     companion object {
         fun from(key: String, rawMap: Map<String, Any>?): IssueData {
@@ -84,7 +113,9 @@ interface JiraIssue {
 
     companion object {
         fun story(issueData: IssueData): Story = Story(key = issueData.key, detail = issueData.detail, title = issueData.getStoryTitle())
+        fun spike(issueData: IssueData): Spike = Spike(key = issueData.key, detail = issueData.detail, title = issueData.getSpikeTitle())
     }
 }
 
 data class Story(override val key: String, override val detail: String, val title: String) : JiraIssue
+data class Spike(override val key: String, override val detail: String, val title: String) : JiraIssue
